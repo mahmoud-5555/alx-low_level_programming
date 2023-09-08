5d40452a647ca018c2684ea6b73a593954e12104
mahmoud-5555 <mahmoudadam5555@gmail.com>
2023-09-08 12:18:55 -0700

    last one

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,10 +8,10 @@
 
 int main(void)
 {
+	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+		;
 
-	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\
-			  n";
-	write(1, message, 59);
+	write(1, message, 58);
+	write(1, "\n", 1);
 	return (1);
-
 }
