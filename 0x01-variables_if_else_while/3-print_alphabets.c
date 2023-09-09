8651f9eff5d554502b14f3ab8dd02244fbe88cc4
mahmoud-5555 <mahmoudadam5555@gmail.com>
2023-09-09 14:26:14 -0700

    1.3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,7 +11,7 @@ int main(void)
 	char myChar[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
 
-	for (i = 0; i < 26; i++)
+	for (i = 0; i < 53; i++)
 	{
 
 		putchar(myChar[i]);
