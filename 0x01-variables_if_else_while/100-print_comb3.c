506d2069a3b88dd8065f7e4131ed6222d2039b34
mahmoud-5555 <mahmoudadam5555@gmail.com>
2023-09-09 17:50:49 -0700

    1.10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, j, x;
+
+	x = 48;
+	for (i = 48; i < 58; i++)
+	{
+		for (j = x; j < 58; j++)
+		{
+			if (i != j)
+			{
+				putchar(i);
+				putchar(j);
+			}
+			if (i != 57 && i != j)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+
+		}
+		x = x + 1;
+	}
+
+	putchar('\n');
+	return (0);
+}
