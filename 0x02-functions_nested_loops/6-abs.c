7837afc110408f2b991eb7d73ec009032a5f14e3
mahmoud-5555 <mahmoudadam5555@gmail.com>
2023-09-12 16:17:30 -0700

    massige

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _abs - Entry point
+ *
+ * @n:the input of the function that will be deal with it
+ *
+ * Return:  (Success)
+ */
+
+int _abs(int n)
+{
+
+	if (n < 0)
+	{
+
+		return (n * -1);
+	}
+	return (n);
+
+
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+
