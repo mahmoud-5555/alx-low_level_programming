186013b828782a5997d07e408fe9fbc4d1d56e34
mahmoud-5555 <mahmoudadam5555@gmail.com>
2023-09-12 16:22:44 -0700

    massige

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_last_digit - Entry point
+ *
+ * @n:the input of the function that will be deal with it
+ *
+ * Return:  (Success)
+ */
+
+int print_last_digit(int n)
+{
+
+
+	return (n % 10);
+
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+
