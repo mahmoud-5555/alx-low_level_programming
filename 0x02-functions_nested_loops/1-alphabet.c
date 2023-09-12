e07417d666dae3667b6d555b03e9b340ae764bdd
mahmoud-5555 <mahmoudadam5555@gmail.com>
2023-09-12 14:24:41 -0700

    2.1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-#include <main.h>
+#include "main.h"
 /**
  * print_alphabet - Entry point
  *
@@ -11,9 +10,9 @@ void print_alphabet(void)
 	int i;
 
 	for (i = 97; i < 123; i++)
-		putchar(i);
+		_putchar(i);
 
-	putchar('\n');
+	_putchar('\n');
 }
 
 /**
