0ade6e48463899a1829f08d1aa42dcddbaaa975d
mahmoud-5555 <mahmoudadam5555@gmail.com>
2023-09-12 12:20:25 -0700

    2.0,2.1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet - Entry point
+ *
+ * Return: no return Always  (Success)
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+		putchar(i);
+
+	putchar('\n');
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	print_alphabet();
+
+	return (0);
+}
