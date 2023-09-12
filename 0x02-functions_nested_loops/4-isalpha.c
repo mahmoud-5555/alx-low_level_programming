7d87a3daf445fd6871514770cf068765c165a0aa
mahmoud-5555 <mahmoudadam5555@gmail.com>
2023-09-12 15:21:10 -0700

    1.4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * _islower - Entry point
+ * __isalpha - Entry point
  *
  * @c:the input of the function that will be check
  *
- * Return: 1 if the char c is lower case  (Success)
+ * Return: 1 if the char c is alphabtic  (Success)
  */
 
-int _islower(int c)
+int _isalpha(int c)
 {
 
 	if ((c > 96 && c < 123) || (c > 64 && c < 133))
