33d9cf64d6fe625a0bb8efa20552de1e849f0059
mahmoud-5555 <mahmoudadam5555@gmail.com>
2023-09-12 13:21:38 -0700

    2.0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include<main.h>
 
 /**
  * main - Entry point
@@ -8,7 +8,11 @@
 
 int main(void)
 {
-	printf("_putchar\n");
+	mychar[]="_putchar\n";
+	int i;
+
+	for (i = 0; i < 7; i++ )
+		_putchar(i);
 
 	return (0);
 }
