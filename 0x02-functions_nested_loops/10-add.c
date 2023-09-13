dd3ffa914fd274faafe450cc576a5b382f18f980
mahmoud-5555 <mahmoudadam5555@gmail.com>
2023-09-13 13:39:29 -0700

    2.10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * add  - Entry point
+ *
+ * @a:the input of the function that will be deal with it
+ * @b:the input of the function that will be deal with it
+ * add two numbers a , b
+ *
+ * Return:  (Success)
+ */
+
+int add(int a, int b)
+{
+
+	return (a + b);
+
+
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+
