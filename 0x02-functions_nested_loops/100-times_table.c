9474b89a7394c8e78b36121857449bc77082acb3
mahmoud-5555 <mahmoudadam5555@gmail.com>
2023-09-13 16:10:39 -0700

    2.12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * print_times_table - Entry point
+ *
+ *@n:  is the int that will use for the argument of the function
+ * Return:  (Success)
+ */
+
+void print_times_table(int n)
+{
+	int j, i, res;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n ; j++)
+		{
+			res = j * i;
+			if (j != n && j != 0)
+			{
+				if (res < 10)
+					printf(" %i, ", res);
+				else
+					printf("%i, ", res);
+			}
+			else if (j == 0)
+			{
+				printf("%i, ", res);
+			}
+			else
+			{
+			if (res < 10)
+				printf(" %i", res);
+			else
+				printf("%i", res);
+			}
+		}
+		printf("\n");
+	}
+}
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
