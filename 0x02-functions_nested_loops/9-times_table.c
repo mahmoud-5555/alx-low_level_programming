7373bb0c99fb481294a52a457ea139df7108f1d2
mahmoud-5555 <mahmoudadam5555@gmail.com>
2023-09-12 19:08:13 -0700

    massige

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * times_table - Entry point
+ *
+ *
+ * Return:  (Success)
+ */
+
+void times_table(void)
+{
+	int j, i, res;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			res = i * j;
+			printf("%i, ", res);
+		}
+		_putchar('\n');
+	}
+}
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
