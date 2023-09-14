6ee126172f7d2f6ac15d6ad76bf969ca6b1ab650
mahmoud-5555 <mahmoudadam5555@gmail.com>
2023-09-14 16:07:00 -0700

    4.4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_most_numbers - Entry point
+ *
+ *
+ *
+ *
+ * Return:  (Success)
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i != '2' || i != '4')
+			_putchar(i);
+	}
+
+	_putchar('\n');
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
