e65553ce7f54a6c7fc4e42633a7cdb4a5d34d512
mahmoud-5555 <mahmoudadam5555@gmail.com>
2023-09-14 14:56:51 -0700

    4.0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,14 +3,14 @@
  * _isupper - Entry point
  *
  * @c:the input of the function that will be deal with it
- * 
+ *
  *
  * Return:  (Success)
  */
 
 int _isupper(int c)
 {
-	if (c > 64 && c < 92)
+	if (c >= 65 && c >=91)
 		return (1);
 	else
 		return (0);
