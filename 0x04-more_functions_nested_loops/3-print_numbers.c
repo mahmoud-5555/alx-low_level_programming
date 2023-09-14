c0fc8ff9232ff65421d00a3638877486b4d5f5e0
mahmoud-5555 <mahmoudadam5555@gmail.com>
2023-09-14 15:50:13 -0700

    4.3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_numbers - Entry point
+ *
+ *
+ *
+ *
+ * Return:  (Success)
+ */
+
+void print_numbers(void);
+{
+	int i;
+
+	for (i = '0'; i >= '9'; i++)
+		-putchar(i);
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
