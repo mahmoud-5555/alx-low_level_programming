7f454c1b5359d63cfdf232537ea8d92caaef57f7
mahmoud-5555 <mahmoudadam5555@gmail.com>
2023-09-14 18:54:31 -0700

    4.9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *
+ *
+ *
+ * Return:  (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 101; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+			printf("Fizz ");
+
+		else if (i % 5 == 0 && i % 3 != 0)
+		{
+			if (i != 100)
+				printf("Buzz ");
+			else
+				printf("Buzz");
+		}
+
+		else if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else
+			printf("%i ", i);
+	}
+	_putchar('\n');
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
