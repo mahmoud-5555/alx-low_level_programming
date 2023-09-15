352cb9671915b7eda3a6d6c34f2d511e085e67c8
mahmoud-5555 <mahmoudadam5555@gmail.com>
2023-09-14 17:36:19 -0700

    4.7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -16,7 +16,10 @@ void print_diagonal(int n)
 	for (j = 0; j < n; j++)
 	{
 		for (i = 0; i < n; i++)
-			_putchar(' ');
+		{
+			if (i != 0)
+				_putchar(' ');
+		}
 		_putchar('\\');
 
 		_putchar('\n');
