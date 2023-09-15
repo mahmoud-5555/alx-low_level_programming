696ae79e92b81600304500ed80b306d316f078b3
mahmoud-5555 <mahmoudadam5555@gmail.com>
2023-09-14 19:46:15 -0700

    2.12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_number - Entry point
+ *
+ *
+ * @n : the input of the function
+ *
+ * Return:  (Success)
+ */
+
+void print_number(int n)
+{
+	unsigned int value;
+
+	if (n < 0)
+	{
+		value = -n;
+		_putchar('-');
+	} else
+	{
+		value = n;
+	}
+
+	if (value / 10)
+	{
+		print_number(value / 10);
+	}
+
+	_putchar((value % 10) + '0');
+
+}
