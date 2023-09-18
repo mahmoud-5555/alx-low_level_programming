7aa76b79d7e868e5fd0b5e0cba1b0730861ad9a3
mahmoud-5555 <mahmoudadam5555@gmail.com>
2023-09-18 13:11:09 -0700

    5.2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - Entry point
+ *
+ * @s: the input that we deal with it
+ *
+ * Return:  (Success)
+ */
+
+int _strlen(char *s)
+{
+
+	int it = 0;
+	int counter = 0;
+
+	while (*(s + it) != '\0')
+		counter++;
+
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
