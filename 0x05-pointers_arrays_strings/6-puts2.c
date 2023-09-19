c779bd5646e2130aaf91b1d22f1ebbfa61ca3d80
mahmoud-5555 <mahmoudadam5555@gmail.com>
2023-09-18 18:12:46 -0700

    5.6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - Entry point
+ *
+ * @str: the input that we deal with it
+ *
+ * Return:  (Success)
+ */
+
+void puts2(char *str)
+{
+	int it = 0;
+
+
+	while (str[it] != '\0')
+	{
+		putchar(str[it]);
+		it += 2;
+	}
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
