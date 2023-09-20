8975611505557641ce67b546c6151c611a6b0846
mahmoud-5555 <mahmoudadam5555@gmail.com>
2023-09-19 19:34:48 -0700

    5.10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * __atoi - function that convite string -> int
+ * _atoi - function that convite string -> int
  *
  *
  * @s: input of the function (to)
@@ -11,32 +11,34 @@
 
 int _atoi(char *s)
 {
-	int ln = 0;
-	int num = 0;
-	int sign = 1;
-	int it = 0;
-	int i;
-	int ordaer = 1;
+	int ln = 0, num = 0, sign = 1, it = 0, i;
+	int order = 1, start = 0, end = 0, s_num = 1;
+	int stop = 1;
 
-	while (*(s + ln) != '\0')
+	if (s[0] == '\0')
+		return (0);
+	while (s[ln] != '\0')
 	{
+		if ((s_num) && ((s[ln] >= '0') && (s[ln] <= '9')))
+		{
+			s_num = 0;
+			start = ln;
+		}
+		if (!((s[ln] >= '0') && (s[ln] <= '9')) &&  !s_num)
+			stop = 0;
+		if (s[ln] == '-')
+			sign *= -1;
+		if (stop)
+			end = ln;
 		ln++;
 	}
-	if (*s == '\0' )
+	if (s_num)
 		return (0);
-
-	else if (*s == '-')
-	{
-		sign = -1;
-		it = 1;
-	}
-
-	for ( ; it < ln; it++)
-	{
-		ordaer = 1;
-		for (i = 0; i < (ln - it -1); i++)
-			ordaer *= 10;
-		num += num + (ordaer * (s[it] - '0'));
+	for (it = start ; it <= end; it++)
+	{		order = 1;
+		for (i = start - (start - it); i < end; i++)
+			order *= 10;
+		num += (order * (s[it] - '0'));
 	}
 	return (num * sign);
 }
