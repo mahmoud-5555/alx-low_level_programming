21515bbfda06eda89e49ea5fce1f4fec0403a501
mahmoud-5555 <mahmoudadam5555@gmail.com>
2023-09-19 19:45:44 -0700

    2.10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -14,7 +14,7 @@ int _atoi(char *s)
 	int ln = 0, sign = 1, it = 0, i;
 	int order = 1, start = 0, end = 0, s_num = 1;
 	int stop = 1;
-	long num = 0;
+	unsigned int  num = 0;
 
 	if (s[0] == '\0')
 		return (0);
