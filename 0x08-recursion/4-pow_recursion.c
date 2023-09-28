5ca253137c9c8b097af0ba9abf658ecef1a4cc36
mahmoud-5555 <mahmoudadam5555@gmail.com>
2023-09-28 15:35:04 -0700

    8.4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+
+/**
+ * _pow_recursion - Entry point
+ *
+ * @x : is base
+ * @y : is the power
+ * Return:  ( int value )  (Success)
+ */
+int _pow_recursion(int x, int y)
+{
+	int res;
+
+	if (y == 0)
+		return (1);
+	else if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y % 2 == 0)
+	{
+		res = _pow_recursion(x, (y / 2));
+		return (res * res);
+	}
+	else if (y % 2 != 0)
+		return (x * _pow_recursion(x, y - 1));
+
+
+}
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
