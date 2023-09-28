bab9c91120288ddd35dd0f122d50187bc6376dbc
mahmoud-5555 <mahmoudadam5555@gmail.com>
2023-09-28 14:08:27 -0700

    8.1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+
+/**
+ * _print_rev_recursion - Entry point
+ *
+ * @s : is string
+ * Return: void ( No return )  (Success)
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_puts_recursion(s + 1);
+		_putchar(*s);
+
+	}
+	else
+		_putchar('\n');
+
+}
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
