3ce7142c58903ea3f982c7e31ed0f8dd2e8beab0
mahmoud-5555 <mahmoudadam5555@gmail.com>
2023-09-28 18:55:55 -0700

    8.5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,6 +2,21 @@
 
 
 /**
+ * _pow_recursion - Entry point
+ * @x : in input
+ * @y : is number thr input of the function
+ * Return:  ( int value the length of string)  (Success)
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y * y == x)
+		return (y);
+	if (y * y > x)
+		return (-1);
+	return (_pow_recursion(x, y + 1));
+}
+
+/**
  * _sqrt_recursion - Entry point
  *
  * @n : is number thr input of the function
@@ -9,23 +24,12 @@
  */
 int _sqrt_recursion(int n)
 {
-	int sqrt_candidate;
-
 	if (n < 0)
-	{
 		return (-1);
-	}
-	else if (n == 0 || n == 1)
+	if (n == 0 || n == 1)
 		return (n);
 
-
-	sqrt_candidate = _sqrt_recursion(n / 2);
-
-	if (sqrt_candidate * sqrt_candidate >= n)
-		return (sqrt_candidate);
-
-	else
-		return (sqrt_candidate + 1);
+	return (_pow_recursion(n, 1));
 
 }
 /**
