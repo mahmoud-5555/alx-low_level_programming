9d4bc69491be8e9a6c0361b39c110c1d69fcf6df
mahmoud-5555 <mahmoudadam5555@gmail.com>
2023-10-03 02:54:51 +0300

    task 9.0

---
+++ 0x09-static_libraries/sours/0-isupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isupper - Entry point
+ *
+ * @c:the input of the function that will be deal with it
+ *
+ *
+ * Return:  (Success)
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
