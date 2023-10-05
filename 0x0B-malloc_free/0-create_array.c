7f132ec1555059cf38fcf005998189226bd877c4
mahmoud-5555 <mahmoudadam5555@gmail.com>
2023-10-05 05:21:53 +0300

    B.0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -19,7 +19,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	char *arr = (char *) malloc(sizeof(char) * size);
+	*arr = (char *) malloc(sizeof(char) * size);
 
 	if (arr == NULL)
 	{
