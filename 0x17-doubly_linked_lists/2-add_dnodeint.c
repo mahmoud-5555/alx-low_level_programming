c6b4de2dafe5521a15d659f392755fc140b2e0fd
mahmoud-5555 <mahmoudadam5555@gmail.com>
2023-12-08 01:44:10 +0200

    0x17 1

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -19,6 +19,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 
 	new_node->n = n;
+	if (*head != NULL)
+		(*head)->prev = new_node;
 	new_node->next = *head;
 	*head = new_node;
 	return (*head);
