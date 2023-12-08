1ca60efd104756dab9f63dc4fbb92f37b2e2125f
mahmoud-5555 <mahmoudadam5555@gmail.com>
2023-12-08 03:48:13 +0200

    0x17 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -40,6 +40,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		{
 			it->prev = new_node;
 			new_node->prev = NULL;
+			*h = new_node;
 		}
 		return (new_node);
 	}
