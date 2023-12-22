5f0a0c59cf8376853173c63e380ac39bf03da9d7
mahmoud-5555 <mahmoudadam5555@gmail.com>
2023-12-23 00:58:04 +0200

    task 0x1A 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -17,7 +17,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (!ht || !key || !value || ht->size == 0)
 		return (0);
-	
+
 	index = key_index((const unsigned char *)key, ht->size);
 	/* If there's already a node in this position */
 	it = ht->array[index];
