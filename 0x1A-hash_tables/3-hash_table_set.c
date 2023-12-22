ca55c5db3884e910f0e9e0b72711d45ee73b745d
mahmoud-5555 <mahmoudadam5555@gmail.com>
2023-12-23 01:01:50 +0200

    task 0x1A 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -32,6 +32,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			it->value = newvalue;
 			return (1);
 		}
+		it = it->next;
 	}
 	new_node = malloc(sizeof(hash_node_t));
 	if (!new_node)
